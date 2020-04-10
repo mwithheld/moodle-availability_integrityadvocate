4c167f52e97c4fe58286ed5f341fe88cfa5100b4
markv <vhmark@gmail.com>
2020-04-10 12:07:28 -0700

    Bump version to 202041000

--- version.php
+++ version.php
@@ -23,6 +23,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 202040800;
+$plugin->version = 202041000;
 $plugin->requires = 2017111300; // Moodle 3.4.0.
 $plugin->component = 'availability_integrityadvocate';
