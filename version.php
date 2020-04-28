ba785ffcc602cbd8e301ef89df049584c8da30ee
markv <vhmark@gmail.com>
2020-04-27 19:59:33 -0700

    Bump version to 2020042700; Bump requires to get updated constants

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020042605;
+$plugin->version = 2020042700;
 $plugin->requires = 2018051700; // Moodle 3.5.0.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 2020042605;
+$plugin->release = 2020042700;
 $plugin->component = 'availability_integrityadvocate';
-$plugin->dependencies = array('block_integrityadvocate' => 2020041600);
+$plugin->dependencies = array('block_integrityadvocate' => 2020042608);
