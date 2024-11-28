59ffa441c6cc9ff4315f2c86656551200b4d4777
mwithheld <796986+mwithheld@users.noreply.github.com>
2024-11-27 18:31:16 -0800

    eslint: ignore no-unused-expressions and allowShortCircuit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
         'no-console': 'off',
         'max-len': 'off',
         // I would like to have this rule on, but have one variable that I want mixed case.
-        'camelcase': 'off'
+        'camelcase': 'off',
+        // Allow code like: debug && window.console().
+        "no-unused-expressions": ["error", { "allowShortCircuit": true }]
     }
 };
